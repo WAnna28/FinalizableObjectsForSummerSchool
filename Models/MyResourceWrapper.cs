20c385d760491f4ccc67ef9178dfa2d620918a01
Anna Hakobyan <Anna.Hakobyan@helpsystems.com>
2021-09-29 03:21:57 +0400

    Add project files.

---
+++ Models/MyResourceWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// // System.Object
+// public class Object
+// {
+// ...
+//  protected virtual void Finalize() { }
+// }
+
+// The role of the Finalize() method is to ensure that a .NET Core
+// object can clean up unmanaged resources when it is garbage collected.
+namespace FinalizableObjectsForSummerSchool.Models
+{
+    class MyResourceWrapper
+    {
+        // Compile-time error!
+        // protected override void Finalize() { }
+
+        // Clean up unmanaged resources here.
+        // Beep when destroyed (testing purposes only!)
+        ~MyResourceWrapper() {
+            Console.WriteLine("test");
+            Console.Beep(); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using FinalizableObjectsForSummerSchool.Models;
+using System;
+
+namespace FinalizableObjectsForSummerSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateObjects(10000);
+
+            // Informs the runtime of a large allocation of unmanaged memory
+            // that should be taken into account when scheduling garbage collection.
+            GC.AddMemoryPressure(2147483647);
+
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+
+            Console.ReadLine();
+        }
+        
+        static void CreateObjects(int count)
+        {
+            MyResourceWrapper[] tonsOfObjects = new MyResourceWrapper[count];
+           
+            for (int i = 0; i < count; i++)
+            {
+                tonsOfObjects[i] = new MyResourceWrapper();
+            }
+
+            tonsOfObjects = null;
+        }
+    }
+}
